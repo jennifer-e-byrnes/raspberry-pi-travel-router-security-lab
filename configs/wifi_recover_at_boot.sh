3e2245d760b77131951c89a1f09615091b572755
Jenn <jenniferbyrnes264@gmail.com>
2026-03-10 16:04:55 -0700

    Create wifi_recover_at_boot.sh for delayed Wi-Fi reload
    Add a script to recover Wi-Fi services after boot with a delay.

--- configs/wifi_recover_at_boot.sh
+++ configs/wifi_recover_at_boot.sh
@@ -1,1 +1,9 @@
+#!/bin/sh
+# Delayed Wi-Fi reload to recover wireless services after boot.
 
+sleep 25
+logger -t wifi-recover "Delayed wifi reload starting"
+wifi down
+sleep 3
+wifi up
+logger -t wifi-recover "Delayed wifi reload finished"
